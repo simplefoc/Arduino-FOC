b774a73bb703d158796fd7b15804b426bd5883d4
askuric <antun.skuric@outlook.com>
2020-04-10 15:21:06 +0200

    minimal example

--- examples/angle_control/angle_control.ino
+++ examples/angle_control/angle_control.ino
@@ -14,20 +14,21 @@ BLDCMotor motor = BLDCMotor(9, 10, 11, 11, 8);
 //  - cpr           - counts per rotation  (cpm=ppm*4)
 //  - index pin     - (optional input)
 Encoder encoder = Encoder(arduinoInt1, arduinoInt2, 32768, 4);
+// interrupt ruotine intialisation
+void doA(){encoder.handleA();}
+void doB(){encoder.handleB();}
 
 void setup() {
   // debugging port
   Serial.begin(115200);
 
-
   // check if you need internal pullups
   // Pullup::EXTERN - external pullup added
   // Pullup::INTERN - needs internal arduino pullup
   encoder.pullup = Pullup::EXTERN;
+  
   // initialise encoder hardware
-  encoder.init();
-
-  encoder.enableInterrupt();
+  encoder.init(doA, doB);
 
   // set driver type
   //  DriverType::unipolar
