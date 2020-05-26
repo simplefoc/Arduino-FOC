ca5d9e3c35aa123d1ff70f0af944d9ca5493d528
askuric <antun.skuric@outlook.com>
2020-05-26 12:30:05 +0200

    FEAT addded the defualts file

---
+++ arduino_foc_minimal_encoder/defaults.h
@@ -0,0 +1,19 @@
+// default configuration values
+// power supply voltage
+#define DEF_POWER_SUPPLY 12.0
+// velocity PI controller params
+#define DEF_PI_VEL_P 0.5
+#define DEF_PI_VEL_I 10
+#define DEF_PI_VEL_U_RAMP 300
+// angle P params
+#define DEF_P_ANGLE_P 20
+// angle velocity limit default
+#define DEF_P_ANGLE_VEL_LIM 20
+// index search velocity
+#define DEF_INDEX_SEARCH_TARGET_VELOCITY 1
+// velocity PI controller params for index search
+#define DEF_PI_VEL_INDEX_P 1
+#define DEF_PI_VEL_INDEX_I 10
+#define DEF_PI_VEL_INDEX_U_RAMP 100
+// velocity filter time constant
+#define DEF_VEL_FILTER_Tf 0.005
