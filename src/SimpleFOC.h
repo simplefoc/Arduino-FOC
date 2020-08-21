e5e607c0119071d8dec5a7e60394f60b09813792
Owen Williams <williams.owen@gmail.com>
2020-08-21 13:26:44 +0100

    adding MagneticSensorAnalog implementation

--- src/SimpleFOC.h
+++ src/SimpleFOC.h
@@ -91,6 +91,7 @@ void loop() {
 #include "Encoder.h"
 #include "MagneticSensorSPI.h"
 #include "MagneticSensorI2C.h"
+#include "MagneticSensorAnalog.h"
 #include "HallSensor.h"
 #include "BLDCMotor.h"
 
