e12b8f7d3bc39e8545f72657ce7b9dc1cc9fc77e
Owen Williams <williams.owen@gmail.com>
2020-08-31 11:05:50 +0100

    Change comments to explain that ESP32 has 12bit ADC (i.e. different max value needed for constructor)

--- src/MagneticSensorAnalog.cpp
+++ src/MagneticSensorAnalog.cpp
@@ -3,7 +3,7 @@
 /** MagneticSensorAnalog(uint8_t _pinAnalog, int _min, int _max)
  * @param _pinAnalog  the pin that is reading the pwm from magnetic sensor
  * @param _min_raw_count  the smallest expected reading.  Whilst you might expect it to be 0 it is often ~15.  Getting this wrong results in a small click once per revolution
- * @param _max_raw_count  the largest value read.  whilst you might expect it to be 2^10 = 1023 it is often ~ 1020
+ * @param _max_raw_count  the largest value read.  whilst you might expect it to be 2^10 = 1023 it is often ~ 1020. Note: For ESP32 (with 12bit ADC the value will be nearer 4096)
  */
 MagneticSensorAnalog::MagneticSensorAnalog(uint8_t _pinAnalog, int _min_raw_count, int _max_raw_count){
   
