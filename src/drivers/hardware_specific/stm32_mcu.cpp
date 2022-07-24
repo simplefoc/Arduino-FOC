c87318caa2812050f6faafc8788432645cd2a208
Richard Unger <richard.unger@nexum.com>
2022-07-24 16:43:01 +0200

    add debug to start/stop timers

--- src/drivers/hardware_specific/stm32_mcu.cpp
+++ src/drivers/hardware_specific/stm32_mcu.cpp
@@ -167,6 +167,9 @@ void _startTimers(HardwareTimer **timers_to_start, int timer_num)
   for (int i=0; i < timer_num; i++) {
     if(timers_to_start[i] == NP) return;
     timers_to_start[i]->resume();
+    #ifdef SIMPLEFOC_STM32_DEBUG
+      SIMPLEFOC_DEBUG("STM32-DRV: Starting timer ", getTimerNumber(get_timer_index(timers_to_start[i]->getHandle()->Instance)));
+    #endif
   }
 }
 
