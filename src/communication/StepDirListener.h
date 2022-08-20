c9e14b670f6c1cda7013d542680f03aadcf3b051
Richard Unger <richard.unger@nexum.com>
2022-08-20 03:45:17 +0200

    fix compile error for Due

--- src/communication/StepDirListener.h
+++ src/communication/StepDirListener.h
@@ -5,7 +5,7 @@
 #include "../common/foc_utils.h"
 
 
-#if defined(_STM32_DEF_) || defined(ESP_H) || defined(ARDUINO_ARCH_AVR)
+#if defined(_STM32_DEF_) || defined(ESP_H) || defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_SAM_DUE)
 #define PinStatus int
 #endif
 
