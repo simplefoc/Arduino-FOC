27415a7b766b074ac4e3466c58ae38eaee197b29
Sudhanta Suryaputra <suryabejibun@gmail.com>
2022-12-31 19:55:59 +0700

    try correcting comment
    I'm not an expert, so please correct me if I'm wrong. But I think Current should be Voltage / Resistance, and it's dangerous if people using wrong formula. So please take a look at this. Thankyou.

--- examples/motion_control/open_loop_motor_control/open_loop_velocity_example/open_loop_velocity_example.ino
+++ examples/motion_control/open_loop_motor_control/open_loop_velocity_example/open_loop_velocity_example.ino
@@ -37,7 +37,7 @@ void setup() {
   // limiting motor movements
   // limit the voltage to be set to the motor
   // start very low for high resistance motors
-  // currnet = resistance*voltage, so try to be well under 1Amp
+  // current = voltage / resistance, so try to be well under 1Amp
   motor.voltage_limit = 3;   // [V]
  
   // open loop control config
