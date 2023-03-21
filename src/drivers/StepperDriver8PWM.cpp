e7e92e7603e7b884c8e68ae282c07ad7a7936112
Juan-Antonio Søren E. Pedersen <Juanpedersen@gmail.com>
2023-03-21 19:04:40 +0100

    dev´s

---
+++ src/drivers/StepperDriver8PWM.cpp
@@ -0,0 +1,102 @@
+#include "StepperDriver8PWM.h"
+
+StepperDriver8PWM::StepperDriver8PWM(int ph1A, int ph1B, int ph2A, int ph2B, int ph3A, int ph3B, int ph4A, int ph4B, int en1, int en2, int en3, int en4) {
+  // Pin initialization
+  pwm1A = ph1A;
+  pwm1B = ph1B;
+  pwm2A = ph2A;
+  pwm2B = ph2B;
+  pwm3A = ph3A;
+  pwm3B = ph3B;
+  pwm4A = ph4A;
+  pwm4B = ph4B;
+
+  // Enable pins
+  enable_pin1 = en1;
+  enable_pin2 = en2;
+  enable_pin3 = en3;
+  enable_pin4 = en4;
+
+  // Default values
+  voltage_power_supply = DEF_POWER_SUPPLY;
+  voltage_limit = NOT_SET;
+  pwm_frequency = NOT_SET;
+
+ 
+
+  pinMode(enable_pin1, OUTPUT);
+  pinMode(enable_pin2, OUTPUT);
+  disable();
+
+    // dead zone initial - 2%
+  dead_zone = 0.02f;
+  
+}
+
+
+
+
+
+
+
+
+// init hardware pins for 8PWM control
+int StepperDriver8PWM::init() {
+
+  // PWM pins
+  pinMode(pwm1A, OUTPUT);
+  pinMode(pwm1B, OUTPUT);
+  pinMode(pwm2A, OUTPUT);
+  pinMode(pwm2B, OUTPUT);
+  pinMode(pwm3A, OUTPUT);
+  pinMode(pwm3B, OUTPUT);
+  pinMode(pwm4A, OUTPUT);
+  pinMode(pwm4B, OUTPUT);
+
+  if( _isset(enable_pin1) ) pinMode(enable_pin1, OUTPUT);
+  if( _isset(enable_pin2) ) pinMode(enable_pin2, OUTPUT);
+
+  // sanity check for the voltage limit configuration
+  if( !_isset(voltage_limit) || voltage_limit > voltage_power_supply) voltage_limit =  voltage_power_supply;
+
+  // Set the pwm frequency to the pins
+  // hardware specific function - depending on driver and mcu
+  params = _configure8PWM(pwm_frequency, dead_zone, pwm1A, pwm1B, pwm2A, pwm2B, pwm3A, pwm3B, pwm4A, pwm4B);
+  initialized = (params!=SIMPLEFOC_DRIVER_INIT_FAILED);  
+  return params!=SIMPLEFOC_DRIVER_INIT_FAILED;
+}
+
+
+
+// Set voltage to the pwm pin for 8PWM control
+void StepperDriver8PWM::setPwm(float Ualpha, float Ubeta) {
+  float duty_cycle1A_h1(0.0f),duty_cycle1A_h2(0.0f),duty_cycle1B_h1(0.0f),duty_cycle1B_h2(0.0f);
+  float duty_cycle2A_h1(0.0f),duty_cycle2A_h2(0.0f),duty_cycle2B_h1(0.0f),duty_cycle2B_h2(0.0f);
+
+  // limit the voltage in driver
+  Ualpha = _constrain(Ualpha, -voltage_limit, voltage_limit);
+  Ubeta = _constrain(Ubeta, -voltage_limit, voltage_limit);
+
+  // hardware specific writing
+  if( Ualpha > 0 ) {
+    duty_cycle1B_h1 = _constrain(abs(Ualpha)/voltage_power_supply,0.0f,1.0f);
+    duty_cycle1B_h2 = 0.0f; // set second half-bridge duty cycle to 0
+  }
+  else {
+    duty_cycle1A_h1 = _constrain(abs(Ualpha)/voltage_power_supply,0.0f,1.0f);
+    duty_cycle1A_h2 = 0.0f; // set second half-bridge duty cycle to 0
+  }
+
+  if( Ubeta > 0 ) {
+    duty_cycle2B_h1 = _constrain(abs(Ubeta)/voltage_power_supply,0.0f,1.0f);
+    duty_cycle2B_h2 = 0.0f; // set second half-bridge duty cycle to 0
+  }
+  else {
+    duty_cycle2A_h1 = _constrain(abs(Ubeta)/voltage_power_supply,0.0f,1.0f);
+    duty_cycle2A_h2 = 0.0f; // set second half-bridge duty cycle to 0
+  }
+
+  // write to hardware
+  _writeDutyCycle8PWM(duty_cycle1A_h1, duty_cycle1A_h2, duty_cycle1B_h1, duty_cycle1B_h2,
+                      duty_cycle2A_h1, duty_cycle2A_h2, duty_cycle2B_h1, duty_cycle2B_h2, params);
+}
---
+++ src/drivers/StepperDriver8PWM.h
@@ -0,0 +1,68 @@
+#ifndef STEPPER_DRIVER_4PWM_h
+#define STEPPER_DRIVER_4PWM_h
+
+#include "../common/base_classes/StepperDriver.h"
+#include "../common/foc_utils.h"
+#include "../common/time_utils.h"
+#include "../common/defaults.h"
+#include "hardware_api.h"
+
+/**
+ 8 pwm stepper driver class
+*/
+class StepperDriver8PWM : public StepperDriver {
+  public:
+    /**
+      StepperMotor class constructor
+      @param ph1A 1A phase pwm pin
+      @param ph1B 1B phase pwm pin
+      @param ph2A 2A phase pwm pin
+      @param ph2B 2B phase pwm pin
+      @param ph3A 3A phase pwm pin
+      @param ph3B 3B phase pwm pin
+      @param ph4A 4A phase pwm pin
+      @param ph4B 4B phase pwm pin
+      @param en1 enable pin phase 1 (optional input)
+      @param en2 enable pin phase 2 (optional input)
+      @param en3 enable pin phase 3 (optional input)
+      @param en4 enable pin phase 4 (optional input)
+    */
+    StepperDriver8PWM(int ph1A, int ph1B, int ph2A, int ph2B, int ph3A, int ph3B, int ph4A, int ph4B, int en1 = NOT_SET, int en2 = NOT_SET, int en3 = NOT_SET, int en4 = NOT_SET);
+    
+    /**  Motor hardware init function */
+    int init() override;
+    /** Motor disable function */
+    void disable() override;
+    /** Motor enable function */
+    void enable() override;
+
+    float dead_zone; //!< a percentage of dead-time(zone) (both high and low side in low) for each pwm cycle [0,1]
+
+    // hardware variables
+    int pwm1A; //!< phase 1A pwm pin number
+    int pwm1B; //!< phase 1B pwm pin number
+    int pwm2A; //!< phase 2A pwm pin number
+    int pwm2B; //!< phase 2B pwm pin number
+    int pwm3A; //!< phase 3A pwm pin number
+    int pwm3B; //!< phase 3B pwm pin number
+    int pwm4A; //!< phase 4A pwm pin number
+    int pwm4B; //!< phase 4B pwm pin number
+    int enable_pin1; //!< enable pin number phase 1
+    int enable_pin2; //!< enable pin number phase 2
+    int enable_pin3; //!< enable pin number phase 3
+    int enable_pin4; //!< enable pin number phase 4
+
+      /** 
+     * Set phase voltages to the harware 
+     * 
+     * @param Ua phase A voltage
+     * @param Ub phase B voltage
+    */
+    void setPwm(float Ua, float Ub) override;
+  private:
+        
+};
+
+
+
+#endif
