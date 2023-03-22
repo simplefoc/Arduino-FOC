cf4bfecc0acacac86047d6ab1eb163a39c2a10dd
Juan-Antonio Søren E. Pedersen <Juanpedersen@gmail.com>
2023-03-22 19:13:19 +0100

    Update stm32_mcu.cpp

--- src/drivers/hardware_specific/stm32/stm32_mcu.cpp
+++ src/drivers/hardware_specific/stm32/stm32_mcu.cpp
@@ -705,57 +705,60 @@ void configure8PWM(void)
     htim1.Init.RepetitionCounter = 0;
     HAL_TIM_PWM_Init(&htim1);
 
+    htim8.Instance = TIM8;
+    htim8.Init.Prescaler = 0;
+    htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim8.Init.Period = 255;
+    htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim8.Init.RepetitionCounter = 0;
+    HAL_TIM_PWM_Init(&htim8);
 
-htim8.Instance = TIM8;
-htim8.Init.Prescaler = 0;
-htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
-htim8.Init.Period = 255;
-htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-htim8.Init.RepetitionCounter = 0;
-HAL_TIM_PWM_Init(&htim8);
+    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+    HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig);
 
-sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig);
+    sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+    sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+    sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+    sBreakDeadTimeConfig.DeadTime = 50;
+    sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+    sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+    sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+    HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig);
+    HAL_TIMEx_ConfigBreakDeadTime(&htim8, &sBreakDeadTimeConfig);
 
-sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
-sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
-sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
-sBreakDeadTimeConfig.DeadTime = 50;
-sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
-sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
-sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
-HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig);
-HAL_TIMEx_ConfigBreakDeadTime(&htim8, &sBreakDeadTimeConfig);
+    // Configure PWM mode for TIM1 channels 1-6
+    TIM_OC_InitTypeDef sConfigOC = {0};
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;
+    sConfigOC.Pulse = 0;
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+    sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
 
-// Configure PWM mode for TIM1 channels 1-6
-TIM_OC_InitTypeDef sConfigOC = {0};
-sConfigOC.OCMode = TIM_OCMODE_PWM1;
-sConfigOC.Pulse = 0;
-sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1);
-HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2);
-HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3);
-HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_4);
-HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_5);
-HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_6);
+    HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1);
+    HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2);
+    HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3);
+    HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_4);
+    HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_5);
+    HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_6);
 
-// Configure PWM mode for TIM8 channel 1
-sConfigOC.OCMode = TIM_OCMODE_PWM1;
-sConfigOC.Pulse = 0;
-sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-HAL_TIM_PWM_ConfigChannel(&htim8, &sConfigOC, TIM_CHANNEL_1);
+    // Configure PWM mode for TIM8 channel 1
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;
+    sConfigOC.Pulse = 0;
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+    sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
 
-// Start PWM output
-HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
-HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
-HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
-HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_5);
-HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_6);
-HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_1);
+    HAL_TIM_PWM_ConfigChannel(&htim8, &sConfigOC, TIM_CHANNEL_1);
+
+    // Start TIM1 and TIM8 PWM signals
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_5);
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_6);
+    HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_1);
 
 
 }
