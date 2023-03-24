fa0ddf3f8c48f287efd7035ca9d162ba530ca3ba
Juan-Antonio Søren E. Pedersen <Juanpedersen@gmail.com>
2023-03-24 20:53:52 +0100

    Update stm32_mcu.cpp

--- src/drivers/hardware_specific/stm32/stm32_mcu.cpp
+++ src/drivers/hardware_specific/stm32/stm32_mcu.cpp
@@ -876,68 +876,41 @@ and they are used for driving push-pull outputs such as MOSFETs.
 
     
 
-    // Configure TIM1 for PWM output
-    TIM1->CR1 |= TIM_CR1_ARPE; // Auto-reload preload enable
-    TIM1->CR1 &= ~TIM_CR1_DIR; // Up counting mode
-    TIM1->CCMR1 |= TIM_CCMR1_OC1PE | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2; // PWM mode 1 on OC1
-    TIM1->CCMR2 |= TIM_CCMR2_OC3PE | TIM_CCMR2_OC4PE | TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3M_2 | TIM_CCMR2_OC4M_1 | TIM_CCMR2_OC4M_2; // PWM mode 1 on OC3 and OC4
-    TIM1->CCER |= TIM_CCER_CC1E | TIM_CCER_CC3E | TIM_CCER_CC4E; // Enable output on OC1, OC3, and OC4
-    TIM1->PSC = 0; // Set prescaler to 0
-    TIM1->ARR = (168000000 / (38000 * 2)) - 1; // Set auto-reload value for 38kHz frequency
-    TIM1->CCR1 = 0; // Set duty cycle to 0%
-    TIM1->CCR2 = 0; // Set duty cycle to 0%
-    TIM1->CCR3 = 0; // Set duty cycle to 0%
-    TIM1->CCR4 = 0; // Set duty cycle to 0%
-
-    GPIOE->AFR[1] &= ~(GPIO_AFRH_AFRH8); // Clear alternate function for PE8
-    GPIOE->AFR[1] |= (0x2 << GPIO_AFRH_AFRH8_Pos); // Set alternate function for PE8
-    GPIOE->AFR[1] &= ~(GPIO_AFRH_AFRH9); // Clear alternate function for PE9
-    GPIOE->AFR[1] |= (0x2 << GPIO_AFRH_AFRH9_Pos); // Set alternate function for PE9
-    GPIOE->AFR[1] &= ~(GPIO_AFRH_AFRH10); // Clear alternate function for PE10
-    GPIOE->AFR[1] |= (0x2 << GPIO_AFRH_AFRH10_Pos); // Set alternate function for PE10
-    GPIOE->AFR[1] &= ~(GPIO_AFRH_AFRH11); // Clear alternate function for PE11
-    GPIOE->AFR[1] |= (0x2 << GPIO_AFRH_AFRH11_Pos); // Set alternate function for PE11
-    GPIOE->AFR[1] &= ~(GPIO_AFRH_AFRH12); // Clear alternate function for PE12
-    GPIOE->AFR[1] |= (0x2 << GPIO_AFRH_AFRH12_Pos); // Set alternate function for PE12
-    GPIOE->AFR[1] &= ~(GPIO_AFRH_AFRH13); // Clear alternate function for PE13
-    GPIOE->AFR[1] |= (0x2 << GPIO_AFRH_AFRH13_Pos); // Set alternate function for PE13
-    TIM1->BDTR |= TIM_BDTR_MOE; // Main output enable
-// Configure CH2 and CH2N on TIM8
-    TIM8->CR1 |= TIM_CR1_ARPE; // Auto-reload preload enable
-    TIM8->CR1 &= ~TIM_CR1_DIR; // Up counting mode
-    TIM8->CR1 &= ~TIM_CR1_CMS; // Set counter mode to center-aligned
-    TIM8->CR1 |= TIM_CR1_CMS_0 | TIM_CR1_CMS_1; // Set counter mode to center-aligned 3
-    TIM8->CCMR1 |= TIM_CCMR1_OC2PE | TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2; // PWM mode 1 on OC2
-    TIM8->CCER |= TIM_CCER_CC2E; // Enable output on OC2
-    TIM8->PSC = 0; // Set prescaler to 0
-    TIM8->ARR = (168000000 / (38000 * 2)) - 1; // Set auto-reload value for 38kHz frequency
-    TIM8->CCR1 = 0; // Set duty cycle to 0%
-    TIM8->CCR2 = 0; // Set duty cycle to 0%
-    TIM8->CCR3 = 0; // Set duty cycle to 0%
-    TIM8->CCR4 = 0; // Set duty cycle to 0%
-    TIM8->BDTR |= TIM_BDTR_MOE; // Main output enable
-
-    GPIOA->AFR[1] &= ~(GPIO_AFRH_AFRH2); // Clear alternate function for PA14
-    GPIOA->AFR[1] |= (0x8 << GPIO_AFRH_AFRH2_Pos); // Set alternate function for PA14
-    GPIOB->AFR[0] &= ~(GPIO_AFRL_AFRL0); // Clear alternate function for PB0
-    GPIOB->AFR[0] |= (0x8 << GPIO_AFRL_AFRL0_Pos); // Set alternate function for PB0
-    TIM8->BDTR |= TIM_BDTR_MOE; // Main output enable
-
-    // Start TIM1 and TIM8 PWM outputs
-    TIM1->EGR |= TIM_EGR_UG; // Generate update event to load new settings
-    TIM8->EGR |= TIM_EGR_UG; // Generate update event to load new settings
-    TIM1->CR1 |= TIM_CR1_CEN; // Enable TIM1 counter
-    TIM8->CR1 |= TIM_CR1_CEN; // Enable TIM8 counter
+    
 
     // Setting ALL pins to LOW
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_12, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_13, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+    if (HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_RESET)!= HAL_OK)
+        {return (STM32DriverParams*)SIMPLEFOC_DRIVER_INIT_FAILED;
+        }else{Serial.println("TIM1_CH1 READY - LOW OK");}
+
+    if (HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, GPIO_PIN_RESET)!= HAL_OK)
+        {return (STM32DriverParams*)SIMPLEFOC_DRIVER_INIT_FAILED;
+        }else{Serial.println("TIM1_CH1N READY - LOW OK");}
+
+    if (HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, GPIO_PIN_RESET)!= HAL_OK)
+        {return (STM32DriverParams*)SIMPLEFOC_DRIVER_INIT_FAILED;
+        }else{Serial.println("TIM1_CH2 READY - LOW OK");}
+
+    if (HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11, GPIO_PIN_RESET)!= HAL_OK)
+        {return (STM32DriverParams*)SIMPLEFOC_DRIVER_INIT_FAILED;
+        }else{Serial.println("TIM1_CH2N READY - LOW OK");}
+
+    if (HAL_GPIO_WritePin(GPIOE, GPIO_PIN_12, GPIO_PIN_RESET)!= HAL_OK)
+        {return (STM32DriverParams*)SIMPLEFOC_DRIVER_INIT_FAILED;
+        }else{Serial.println("TIM1_CH3 READY - LOW OK");}
+
+    if (HAL_GPIO_WritePin(GPIOE, GPIO_PIN_13, GPIO_PIN_RESET)!= HAL_OK)
+        {return (STM32DriverParams*)SIMPLEFOC_DRIVER_INIT_FAILED;
+        }else{Serial.println("TIM1_CH3N READY - LOW OK");}
+
+    if (HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, GPIO_PIN_RESET)!= HAL_OK)
+        {return (STM32DriverParams*)SIMPLEFOC_DRIVER_INIT_FAILED;
+        }else{Serial.println("TIM8_CH2 READY - LOW OK");}
+
+
+    if (HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET)!= HAL_OK)
+        {return (STM32DriverParams*)SIMPLEFOC_DRIVER_INIT_FAILED;
+        }else{Serial.println("TIM8_CH2N READY - LOW OK");}
 
         
       return NULL;
@@ -958,11 +931,6 @@ Furthermore, the second part of the code relies on the first part to have starte
 the code next to each other to ensure that the timers are properly initialized and configured for PWM output.
 
 */
-
-
-
-
-        
 
 
 
@@ -1010,12 +978,7 @@ void _writeDutyCycle4PWM(float dc_1a,  float dc_1b, float dc_2a, float dc_2b, vo
 
 
 
-
-
-
-
-
-   int period;
+   int period = 4359;
        
 
    void _writeDutyCycle8PWM(float duty_cycle1_h1, float duty_cycle1_h2, float duty_cycle2_h1, float duty_cycle2_h2,
