ef580d79ebc5ae7fb913584b2bb923e9df2b9775
Candas1 <candascraig@hotmail.com>
2023-12-17 11:12:28 +0100

    Prevent double constants

--- src/current_sense/LowsideCurrentSense.cpp
+++ src/current_sense/LowsideCurrentSense.cpp
@@ -157,9 +157,9 @@ int LowsideCurrentSense::driverAlign(float voltage){
         // read the current 50 times
         for (int i = 0; i < 100; i++) {
             PhaseCurrent_s c1 = getPhaseCurrents();
-            c.a = c.a*0.6 + 0.4f*c1.a;
-            c.b = c.b*0.6 + 0.4f*c1.b;
-            c.c = c.c*0.6 + 0.4f*c1.c;
+            c.a = c.a*0.6f + 0.4f*c1.a;
+            c.b = c.b*0.6f + 0.4f*c1.b;
+            c.c = c.c*0.6f + 0.4f*c1.c;
             _delay(3);
         }
         driver->setPwm(0, 0, 0);
@@ -204,9 +204,9 @@ int LowsideCurrentSense::driverAlign(float voltage){
         // read the adc voltage 500 times ( arbitrary number )
         for (int i = 0; i < 100; i++) {
             PhaseCurrent_s c1 = getPhaseCurrents();
-            c.a = c.a*0.6 + 0.4f*c1.a;
-            c.b = c.b*0.6 + 0.4f*c1.b;
-            c.c = c.c*0.6 + 0.4f*c1.c;
+            c.a = c.a*0.6f + 0.4f*c1.a;
+            c.b = c.b*0.6f + 0.4f*c1.b;
+            c.c = c.c*0.6f + 0.4f*c1.c;
             _delay(3);
         }
         driver->setPwm(0, 0, 0);
