bd83388bd52f47701f7600b47384abe05ae888ab
Richard Unger <richard.unger@nexum.com>
2024-02-10 03:04:32 +0100

    add a angle_nocascade mode

--- src/common/base_classes/FOCMotor.h
+++ src/common/base_classes/FOCMotor.h
@@ -29,7 +29,8 @@ enum MotionControlType : uint8_t {
   velocity          = 0x01,     //!< Velocity motion control
   angle             = 0x02,     //!< Position/angle motion control
   velocity_openloop = 0x03,
-  angle_openloop    = 0x04
+  angle_openloop    = 0x04,
+  angle_nocascade   = 0x05      //!< Position/angle motion control without velocity cascade
 };
 
 /**
