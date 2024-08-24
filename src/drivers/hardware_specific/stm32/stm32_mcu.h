1d1c5ddec80e9365cae19e4c518ec1eeb5e5097d
Richard Unger <runger1@gmail.com>
2024-08-24 22:57:07 +0200

    HAL / LL only PWM driver

--- src/drivers/hardware_specific/stm32/stm32_mcu.h
+++ src/drivers/hardware_specific/stm32/stm32_mcu.h
@@ -1,8 +1,28 @@
-#ifndef STM32_DRIVER_MCU_DEF
-#define STM32_DRIVER_MCU_DEF
+#pragma once
+
 #include "../../hardware_api.h"
 
-#if defined(_STM32_DEF_)
+#if defined(_STM32_DEF_) || defined(TARGET_PORTENTA_H7)
+
+#ifndef SIMPLEFOC_STM32_MAX_TIMERSUSED
+#define SIMPLEFOC_STM32_MAX_TIMERSUSED 6
+#endif
+#ifndef SIMPLEFOC_STM32_MAX_MOTORSUSED
+#define SIMPLEFOC_STM32_MAX_MOTORSUSED 4
+#endif
+
+
+#ifndef SIMPLEFOC_STM32_DEBUG
+// comment me out to disable debug output
+#define SIMPLEFOC_STM32_DEBUG
+#endif
+
+#if defined(__MBED__)
+#define PinMap_TIM PinMap_PWM
+#define ALTX_MASK 0
+#endif
+
+
 
 // default pwm parameters
 #define _PWM_RESOLUTION 12 // 12bit
@@ -17,19 +37,34 @@
 
 
 typedef struct STM32DriverParams {
-  HardwareTimer* timers[6] = {NULL};
+  TIM_HandleTypeDef* timers_handle[6] = {NULL};
   uint32_t channels[6];
+  uint32_t llchannels[6];
   long pwm_frequency;
+  uint8_t num_timers;
+  TIM_HandleTypeDef* master_timer = NULL;
   float dead_zone;
   uint8_t interface_type;
 } STM32DriverParams;
 
-// timer synchornisation functions
-void _stopTimers(HardwareTimer **timers_to_stop, int timer_num=6);
-void _startTimers(HardwareTimer **timers_to_start, int timer_num=6);
 
-// timer query functions
-bool _getPwmState(void* params);
+// timer allocation functions
+int stm32_getNumTimersUsed();
+int stm32_getNumMotorsUsed();
+STM32DriverParams* stm32_getMotorUsed(int index);
+bool stm32_isTimerUsed(TIM_HandleTypeDef* timer);
+bool stm32_isChannelUsed(PinMap* pin);
+TIM_HandleTypeDef* stm32_getTimer(PinMap* timer);
+TIM_HandleTypeDef* stm32_useTimer(PinMap* timer);
 
-#endif
+void stm32_pause(STM32DriverParams* params);
+void stm32_resume(STM32DriverParams* params);
+
+// // timer synchornisation functions
+// void _stopTimers(TIM_HandleTypeDef **timers_to_stop, int timer_num=6);
+// void _startTimers(TIM_HandleTypeDef **timers_to_start, int timer_num=6);
+
+// // timer query functions
+// bool _getPwmState(void* params);
+
 #endif
